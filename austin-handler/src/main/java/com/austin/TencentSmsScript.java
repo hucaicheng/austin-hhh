60fb4b15b54e811ff35a4a90d04be461b6c8fc14
Administrator <1056959954@qq.com>
2023-07-07 15:18:15 +0800

    引入腾讯云下发短信代码

---
+++ austin-handler/src/main/java/com/austin/TencentSmsScript.java
@@ -0,0 +1,57 @@
+package com.austin;
+
+import com.tencentcloudapi.common.Credential;
+import com.tencentcloudapi.common.profile.ClientProfile;
+import com.tencentcloudapi.common.profile.HttpProfile;
+import com.tencentcloudapi.common.exception.TencentCloudSDKException;
+import com.tencentcloudapi.sms.v20210111.SmsClient;
+import com.tencentcloudapi.sms.v20210111.models.*;
+
+/**
+ * 下发短信(腾讯)
+ * 1. 通过sdk的方式发送
+ */
+public class TencentSmsScript {
+
+    /**
+     *
+     * @param phoneNumber 手机号
+     * @param SmsSdkAppId 短信应用应用
+     * @param SignName 短信签名内容
+     * @param TemplateId 短信模板id
+     */
+    public static void sendTencentSmsScript(String phoneNumber, String SmsSdkAppId ,String SignName, String TemplateId){
+        try{
+            // 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
+            // 代码泄露可能会导致 SecretId 和 SecretKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议采用更安全的方式来使用密钥，请参见：https://cloud.tencent.com/document/product/1278/85305
+            // 密钥可前往官网控制台 https://console.cloud.tencent.com/cam/capi 进行获取
+            Credential cred = new Credential("SecretId", "SecretKey");
+            // 实例化一个http选项，可选的，没有特殊需求可以跳过
+            HttpProfile httpProfile = new HttpProfile();
+            httpProfile.setEndpoint("sms.tencentcloudapi.com");
+            // 实例化一个client选项，可选的，没有特殊需求可以跳过
+            ClientProfile clientProfile = new ClientProfile();
+            clientProfile.setHttpProfile(httpProfile);
+            // 实例化要请求产品的client对象,clientProfile是可选的
+            SmsClient client = new SmsClient(cred, "ap-beijing", clientProfile);
+            // 实例化一个请求对象,每个接口都会对应一个request对象
+            SendSmsRequest req = new SendSmsRequest();
+            String[] phoneNumberSet1 = {"18702515354"};
+            req.setPhoneNumberSet(phoneNumberSet1);
+
+            req.setSmsSdkAppId("1400836675");
+            req.setSignName("今年何夕08");
+            req.setTemplateId("1856652");
+            // 返回的resp是一个SendSmsResponse的实例，与请求对象对应
+            SendSmsResponse resp = client.SendSms(req);
+            // 输出json格式的字符串回包
+            System.out.println(SendSmsResponse.toJsonString(resp));
+        } catch (TencentCloudSDKException e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    public static void main(String [] args) {
+        sendTencentSmsScript("18702515354","1400836675","今年何夕08","1856652");
+    }
+}
